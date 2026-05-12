4fc0d862f9edc5a592440890dbc036b1a91b7a42
Ashish269 <ashishkumar183183@gmail.com>
2026-05-13 00:54:04 +0530

    CHORE: initial project setup

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# CHIRPO - Where ideas vibe together.
+
+
+### ESLint Setup 
+[ESLint Setup Article](https://medium.com/@sindhujad6/setting-up-eslint-and-prettier-in-a-node-js-project-f2577ee2126f)
---
+++ backend/eslint.config.js
@@ -0,0 +1,25 @@
+import pluginJs from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+
+export default defineConfig([
+  pluginJs.configs.recommended,
+
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+
+    languageOptions: {
+      globals: globals.node,
+    },
+
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+]);
