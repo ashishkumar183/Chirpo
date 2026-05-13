7d1f190e00365a38a01d2e3f3c1bac8b877a615f
Ashish269 <ashishkumar183183@gmail.com>
2026-05-13 13:01:19 +0530

    FEAT:Added basis routing layer

---
+++ backend/src/routes/apiRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import v1Router from './v1/v1Router.js';
+
+const router = express.Router();
+
+router.use('/v1', v1Router);
+
+router.get('/hello', (req, res) => {
+  res.send('hello api');
+});
+
+export default router;
